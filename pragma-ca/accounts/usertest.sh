54001c587c696ed125d69627eb74a63d211755b8
nadyawilliams <nadya@sdsc.edu>
2016-06-07 13:41:11 -0700

    pragma CA records

---
+++ pragma-ca/accounts/usertest.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+grid-proxy-init
+globus-job-run sakura.hpcc.jp/jobmanager /bin/hostname
+globus-job-run pragma001.grid.sinica.edu.tw/jobmanager /bin/hostname
+globus-job-run marlin.bii.a-star.edu.sg/jobmanager /bin/hostname
+globus-job-run pop.cs.binghamton.edu/jobmanager /bin/hostname
+globus-job-run pragma.sdg.ac.cn/jobmanager /bin/hostname
+globus-job-run server1.itsc.cuhk.edu.hk/jobmanager /bin/hostname
+globus-job-run gdlab.gscas.ac.cn/jobmanager /bin/hostname
+globus-job-run supernode2.hcmut.edu.vn/jobmanager /bin/hostname
+globus-job-run bkluster.hpcc.hut.edu.vn/jobmanager /bin/hostname
+globus-job-run exodus.ihpc.a-star.edu.sg/jobmanager /bin/hostname
+globus-job-run venus.ioit-hcm.ac.vn/jobmanager /bin/hostname
+globus-job-run grid1.jlu.edu.cn/jobmanager /bin/hostname
+globus-job-run jupiter.gridcenter.or.kr/jobmanager /bin/hostname
+globus-job-run pragma.lzu.edu.cn/jobmanager /bin/hostname
+globus-job-run nucleus.mygridusbio.net.my/jobmanager /bin/hostname
+globus-job-run mahar.csse.monash.edu.au/jobmanager /bin/hostname
+globus-job-run nacona00.nchc.org.tw/jobmanager /bin/hostname
+globus-job-run tgc.trecc.org/jobmanager /bin/hostname
+globus-job-run grid64.hpcc.nectec.or.th/jobmanager /bin/hostname
+globus-job-run guava.ngpp.ngp.org.sg/jobmanager /bin/hostname
+globus-job-run cafe01.exp-net.osaka-u.ac.jp/jobmanager /bin/hostname
+globus-job-run tea01.exp-net.osaka-u.ac.jp/jobmanager /bin/hostname
+globus-job-run rocks-52.sdsc.edu/jobmanager /bin/hostname
+globus-job-run rocks-153.sdsc.edu/jobmanager /bin/hostname
+globus-job-run rocks-96.sdsc.edu/jobmanager /bin/hostname
+globus-job-run sunyata.thaigrid.or.th/jobmanager /bin/hostname
+globus-job-run syntagma.dim.uchile.cl/jobmanager /bin/hostname
+globus-job-run malicia.super.unam.mx/jobmanager /bin/hostname
+globus-job-run amber.uohyd.ernet.in/jobmanager /bin/hostname
+globus-job-run volatile.ece.uprm.edu/jobmanager /bin/hostname
+globus-job-run komolongma.ece.uprm.edu/jobmanager /bin/hostname
+globus-job-run aurora.cs.usm.my/jobmanager /bin/hostname
+globus-job-run hawk.usm.my/jobmanager /bin/hostname
+globus-job-run ocikbpra.unizh.ch/jobmanager /bin/hostname
